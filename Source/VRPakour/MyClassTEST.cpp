2368c2f870375ac1517ae450bf7a1309682dece4
Quinn McGregor <quinn1115@users.noreply.github.com>
2019-03-18 13:45:54 +0100

    test

---
+++ Source/VRPakour/MyClassTEST.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyClassTEST.h"
+
+MyClassTEST::MyClassTEST()
+{
+}
+
+MyClassTEST::~MyClassTEST()
+{
+}
---
+++ Source/VRPakour/VRPakour.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRPakour.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRPakour, "VRPakour" );
